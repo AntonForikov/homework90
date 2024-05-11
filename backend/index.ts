0c6cd1724e12a5c34758f6d5c4ff77b146678dab
Anton Forikov <tohabass@gmail.com>
2024-05-11 15:04:05 +0600

    backend: realise chat for webSocket understanding

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import expressWs from "express-ws";
 import cors from "cors";
+import {ActiveConnections, IncomingMessage} from "./types";
 
 const app = express();
 expressWs(app);
@@ -11,12 +12,30 @@ app.use(cors());
 
 const router = express.Router();
 
-router.ws('/paint', (ws, req) => {
-  console.log('client connected');
+const activeConnections: ActiveConnections = {}
 
-  ws.on('close', () => {
-      console.log('client disconnected');
-  });
+router.ws('/paint', (ws, req) => {
+    const id = crypto.randomUUID();
+    console.log(`client connected! id - ${id}`);
+    activeConnections[id] = ws;
+    let username = 'Anonymous';
+
+    ws.on('message', (message) => {
+        const parsedMessage = JSON.parse(message.toString()) as IncomingMessage;
+        if (parsedMessage.type === 'SET_USERNAME') {
+            username = parsedMessage.payload;
+        } else if (parsedMessage.type === 'SEND_MESSAGE') {
+            Object.values(activeConnections).forEach((connection) => {
+                const outgoingMessage = {type: 'NEW_MESSAGE', payload: parsedMessage.payload};
+                connection.send(JSON.stringify(outgoingMessage));
+            })
+        }
+    });
+
+    ws.on('close', () => {
+        console.log(`client disconnected. id - ${id}`);
+        delete activeConnections[id];
+    });
 });
 
 app.use(router);
---
+++ backend/types.d.ts
@@ -0,0 +1,11 @@
+import {WebSocket} from 'ws';
+
+export interface ActiveConnections {
+    [id: string]: WebSocket
+}
+
+export interface IncomingMessage {
+    type: string;
+    payload: string;
+}
+
